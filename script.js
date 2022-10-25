a6b4464d807b650266378a8c65dab756d091e55b
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2022-10-25 18:14:06 +0100

    25/10/2022

--- script.js
+++ script.js
@@ -1,7 +1,77 @@
-const numberbuttons = document.querySelectorAll('[data-number]')
-const operationbuttons = document.querySelectorAll('[data-operation]')
-const equalsbutton = docuement.querySelector('[data-equals]')
-const deletebutton = docuement.querySelector('[data-delete]')
-const allClearbutton = docuement.querySelector('[data-all-clear]')
-const previousOperandtextElement = docuement.querySelector('[data-previous-operand]')
-const currentOperandtextElement= docuement.querySelector('[data-current-operand]')
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+   this.previousOperandTextElement = previousOperandTextElement 
+   this.currentOperandTextElement = currentOperandTextElement
+   this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = '' 
+        this.operation = undefined
+    }
+
+    delete(){
+    }
+
+    appendNumber(number){
+        if(number ===  '.' && this.currentOperand.includes( '.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    } 
+
+
+    chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        
+        this.operation = operation 
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+
+    compute(){
+    }
+   
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand 
+       this.previousOperandTextElement.innerText = this.previousOperand
+    }
+}
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement= document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button => {
+button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText) 
+    calculator.updateDisplay()
+})
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText) 
+        calculator.updateDisplay()
+ })
+ })
+
+
+ equalsButton.addEventListener('click', () => {
+        calculator.compute() 
+        calculator.updateDisplay()
+ })
+ 
+    
